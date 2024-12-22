5ba2db13233a0cf631374be0f9aa19b4be56bbc7
HashimAlsarraj <alsarraj.hashim@std.izu.edu.tr>
2024-12-22 19:15:05 +0300

    Generation and Collusions physics
    added code for collusions and model rigged body

---
+++ Assets/Scripts/Generation/DesertGeneration.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesertGeneration : MonoBehaviour
+{
+    public GameObject[] blockSection;
+    public int numberGen;
+    public int zPos = 0;
+    
+    public GameObject doozPlay;
+    
+    public GameObject fakeCam;
+    public int pickChar;
+    public bool generatingBlock = false;
+
+    void Start()
+    {
+        
+        //pickChar = Random.Range(1, 6); //change to 6 for all
+
+
+        if (pickChar == 0)
+        {
+            doozPlay.SetActive(true);
+        }
+       
+        fakeCam.SetActive(false);
+        numberGen = Random.Range(0, 10);
+        blockSection[numberGen].SetActive(true);
+        Instantiate(blockSection[numberGen], new Vector3(0, 0, zPos), Quaternion.identity);
+        blockSection[numberGen].SetActive(false);
+        zPos += 200;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (generatingBlock == false)
+        {
+            generatingBlock = true;
+            StartCoroutine(GenerateNext());
+        }
+    }
+
+
+    IEnumerator GenerateNext()
+    {
+        yield return new WaitForSeconds(1);
+        numberGen = Random.Range(0, 10);
+        blockSection[numberGen].SetActive(true);
+        Instantiate(blockSection[numberGen], new Vector3(0, 0, zPos), Quaternion.identity);
+        blockSection[numberGen].SetActive(false);
+        zPos += 200;
+        transform.position = new Vector3(0, 3, zPos - 200);
+        generatingBlock = false;
+    }
+
+}
